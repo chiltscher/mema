3d3a3064f099192fe8d09b032f15ed2051d14913
chiltscher <hiltscher.christian@gmail.com>
2018-12-07 11:23:40 +0100

    added background and view engine

---
+++ src/Index.ts
@@ -0,0 +1,5 @@
+import {app} from "./app/Application";
+
+app.listen(2208, () => {
+    console.log("Server started @ 2208");
+});
--- src/app/Application.ts
+++ src/app/Application.ts
@@ -1,10 +1,17 @@
 import * as express from "express";
 import { MemberController } from "../controller/MemberController";
 import {LoginController} from "../controller/LoginController";
+import {join, resolve} from "path";
 
 const app = express();
 
-app.use(LoginController);
+app.use("/assets",express.static(resolve(join(__dirname, "..", "..", "assets"))));
+app.set("view engine", "pug");
+app.get("/", (req, res) => {
+   res.render("welcome");
+});
+
+// app.use(LoginController);
 app.use("/member", MemberController);
 
 export { app };
